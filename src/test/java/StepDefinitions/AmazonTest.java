5b7271f8464b11696f17d915f2323df8f968d997
Surabhi <surabhiparab1993@gmail.com>
2022-04-24 17:20:58 +0100

    Implemented code to clear the basket

--- src/test/java/StepDefinitions/AmazonTest.java
+++ src/test/java/StepDefinitions/AmazonTest.java
@@ -27,6 +27,7 @@ import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -52,8 +53,6 @@ public class AmazonTest {
 		CookiePage.customiseCookieButton();
 		CookiePage.savePreferencesButton();
 
-//		driver.findElement(By.id("sp-cc-customize")).click();
-//		driver.findElement(By.className("a-button-input")).click();
 		Thread.sleep(3000);
 	}
 
@@ -69,12 +68,10 @@ public class AmazonTest {
 		LoginPage.enterPassword("AutomationTestUK.");
 		LoginPage.clickSubmitButton();
 
-//		driver.findElement(By.xpath("//div[@id=\"nav-tools\"]/a[2]")).click();
-//		driver.findElement(By.id("ap_email")).sendKeys("emeaintegrationautomation@outlook.com");
-//		driver.findElement(By.id("continue")).click();
-//		driver.findElement(By.id("ap_password")).sendKeys("AutomationTestUK.");
-//		driver.findElement(By.id("signInSubmit")).click();
 		Thread.sleep(3000);
+
+		// Clear the existing items from basket
+		clearBasket();
 
 	}
 
@@ -86,9 +83,6 @@ public class AmazonTest {
 		// Search for the product
 		SearchProduct.searchProductInTextBox("HP Laptop");
 		SearchProduct.clickSearchButton();
-
-//		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("HP Laptop");
-//		driver.findElement(By.id("nav-search-submit-button")).click();
 
 		// Validate search results
 		String searchResults = SearchProduct.validateSearchResults();
@@ -108,6 +102,7 @@ public class AmazonTest {
 		System.out.println("add_the_product_to_basket() : waiting for 30 seconds");
 		Thread.sleep(30000);
 		System.out.println("add_the_product_to_basket() : done waiting for 30 seconds");
+
 		// Add the product to basket
 		WebElement productToSelect = AddProductInBasket.searchProduct(
 				"HP EliteBook 840 G2 14-inch Ultrabook Laptop PC (Intel Core i5-5200U, 8GB RAM, 256GB SSD, WiFi, WebCam, Windows 10 Professional 64-bit)(Renewed)");
@@ -117,17 +112,6 @@ public class AmazonTest {
 
 		AddProductInBasket.doNotAccept();
 
-//		WebElement product = driver.findElement(By.linkText("HP 14s-fq0015na 14\" Full HD Laptop AMD Athlon 3020E 4GB RAM 128GB SSD Windows 10 Home S Mode, White"));
-//		productSelected = product.getText();
-//		product.click();
-//		driver.findElement(By.id("add-to-cart-button")).click();
-
-//		if ( driver.findElement(By.id("attach-desktop-sideSheet")).isEnabled())	{
-//			
-//				//driver.findElement(By.id("attach-popover-lgtbox")).click();
-//			driver.findElement(By.xpath("(//span[@id=\"attachSiNoCoverage\"]//input)[1]")).click();
-//		}
-//		Thread.sleep(5000);
 	}
 
 	@Then("validate the product is added to basket")
@@ -135,7 +119,6 @@ public class AmazonTest {
 
 		ValidateProductAddToBasket ValidateProductAddToBasket = new ValidateProductAddToBasket(driver);
 
-		// Action action = new Action(driver);
 		Thread.sleep(10000);
 		// Open the basket
 		try {
@@ -145,8 +128,6 @@ public class AmazonTest {
 			ValidateProductAddToBasket.openTheBasket();
 		}
 
-		// driver.findElement(By.xpath("(//span[@class='a-button-inner']//a)[1]")).click();
-
 		// Comparing the actual and expected subtotal to ensure item is added in basket
 		String basketProductName = ValidateProductAddToBasket.productInTheBasket();
 		if (productSelected.equals(basketProductName)) {
@@ -154,12 +135,6 @@ public class AmazonTest {
 		} else
 			System.out.println("They do not match");
 
-//		String basketProductName = driver.findElement(By.xpath("//div[@class=\"sc-list-item-content\"]//span[@class=\"a-truncate-cut\"]")).getText();
-//		if(productSelected.equals(basketProductName)) {
-//			System.out.println("They both match");
-//		}
-//		else
-//			System.out.println("They do not match");		
 	}
 
 	@And("increase the quantity and validate the subtotal")
@@ -171,34 +146,42 @@ public class AmazonTest {
 		ValidateSubTotalInBasket.clickOnQuantityDropdown();
 		ValidateSubTotalInBasket.increaseTheQuantityOfProduct();
 		Thread.sleep(10000);
-//		driver.findElement(By.id("a-autoid-1-announce")).click();
-//		driver.findElement(By.xpath("(//li[@class=\"a-dropdown-item quantity-option\"]//a[@id=\"quantity_2\"])"))
-//				.click();
 
 		// Validate the quantity is increased
 		int itemQty = ValidateSubTotalInBasket.getItemQty();
 		int subTotalQty = ValidateSubTotalInBasket.getSubTotalQty();
-		
-		Assert.assertEquals(subTotalQty, itemQty);
-		
-		driver.close();
-		
-		System.out.println("Amazon Test completed. Thank you");
 
-		// String quantity =
-		// driver.findElement(By.className("a-dropdown-prompt")).getText();
-		// String subTotal =
-		// driver.findElement(By.xpath("//span[@id=\"sc-subtotal-label-activecart\"]")).getText();
+		Assert.assertEquals(subTotalQty, itemQty);
+
+		driver.close();
+
+		System.out.println("Amazon Test completed. Thank you");
 
 	}
 
 	public void pressEscape() throws AWTException {
 		Actions action = new Actions(driver);
-		action.sendKeys(Keys.ESCAPE).build().perform();;
-//		action.keyDown(Keys.ESCAPE);
-//		action.keyUp(Keys.ESCAPE);
+		action.sendKeys(Keys.ESCAPE).build().perform();
 
 		System.out.println("Pressed ESC button");
+
+	}
+
+	public void clearBasket() throws InterruptedException {
+
+		driver.findElement(By.xpath("//div[@id='nav-cart-text-container']")).click();
+		try {
+			if (driver.getPageSource().contains(productSelected)) {
+				System.out.println("Cart is empty");
+			}
+		}catch(Exception e) {
+			List<WebElement> itemsInBasket = driver.findElements(By.xpath("//input[@value='Delete']"));
+			while(! itemsInBasket.isEmpty()) {
+				itemsInBasket.get(0).click();
+				Thread.sleep(5000);
+				itemsInBasket = driver.findElements(By.xpath("//input[@value='Delete']"));
+			}
+		}
 
 	}
 
